d38a0e9f914aaccddaca28a664cd8ef7ff56df86
pythoninja <pythoninja@users.noreply.github.com>
2020-10-22 09:56:51 +0300

    refactor: minimize person generator

--- app/models/schema/person.py
+++ app/models/schema/person.py
@@ -5,34 +5,24 @@ from pydantic import BaseModel
 
 
 class PersonSchema(BaseModel):
-    academic_degree: str
     age: int
-    avatar: str
-    blood_type: str
     email: str
     first_name: str
     full_name: str
     gender: str
     height: str
     identifier: str
-    language: str
     last_name: str
-    name: str
     nationality: str
     occupation: str
     password: str
     political_views: str
-    sexual_orientation: str
-    social_media_profile: str
-    surname: str
     telephone: str
     title: str
     university: str
     username: str
-    views_on: str
     weight: int
     work_experience: int
-    worldview: str
     patronymic: Optional[str] = None
     inn: Optional[str] = None
     kpp: Optional[str] = None
