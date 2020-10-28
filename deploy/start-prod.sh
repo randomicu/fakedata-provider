ac84eb63f600c53748eb256e59869f80941af915
pythoninja <pythoninja@users.noreply.github.com>
2020-10-28 16:10:05 +0300

    refactor: move migrations away

--- deploy/start-prod.sh
+++ deploy/start-prod.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 sleep 5
-yoyo apply -vvv --batch --database "$FAKEDATA_DATABASE_URL"
 
 gunicorn -k uvicorn.workers.UvicornWorker -c gunicorn_conf.py app.main:app
--- deploy/start.sh
+++ deploy/start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 sleep 5
-yoyo apply -vvv --batch --database "$FAKEDATA_DATABASE_URL"
 
 uvicorn app.main:app --reload --workers 1 --host 0.0.0.0 --port 8000
