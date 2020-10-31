621551ba1fbb61224a1390686ef6135c13b5899d
pythoninja <pythoninja@users.noreply.github.com>
2020-10-31 21:49:04 +0300

    test: add test to healthcheck endpoint

--- tests/test_healthcheck_router.py
+++ tests/test_healthcheck_router.py
@@ -2,70 +2,14 @@
 from requests import Response
 
 
-def test_en_person_router(client):
+def test_healthcheck_router(client):
     with client:
-        response: Response = client.get('/v1/en/person')
+        response: Response = client.get('/healthcheck')
 
     response_structure = {
-        'age': '',
-        'email': '',
-        'first_name': '',
-        'full_name': '',
-        'gender': '',
-        'height': '',
-        'identifier': '',
-        'last_name': '',
-        'nationality': '',
-        'occupation': '',
-        'password': '',
-        'political_views': '',
-        'telephone': '',
-        'title': '',
-        'university': '',
-        'username': '',
-        'weight': '',
-        'work_experience': ''
+        'status': 'UP'
     }
 
     assert response.status_code == 200
     assert response_structure.keys() == response.json().keys()
-
-
-def test_ru_person_router(client):
-    with client:
-        response: Response = client.get('/v1/ru/person')
-    response_structure = {
-        'age': '',
-        'email': '',
-        'first_name': '',
-        'full_name': '',
-        'gender': '',
-        'height': '',
-        'identifier': '',
-        'last_name': '',
-        'nationality': '',
-        'occupation': '',
-        'password': '',
-        'political_views': '',
-        'telephone': '',
-        'title': '',
-        'university': '',
-        'username': '',
-        'weight': '',
-        'work_experience': '',
-        'patronymic': '',
-        'inn': '',
-        'kpp': '',
-        'bic': '',
-        'ogrn': '',
-        'passport': ''
-    }
-
-    assert response.status_code == 200
-    assert response_structure.keys() == response.json().keys()
-
-
-def test_incorrect_locale(client):
-    with client:
-        response: Response = client.get('/v1/unknown/person')
-    assert response.status_code == 400
+    assert response_structure == response.json()
